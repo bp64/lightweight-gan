fe9b2c631057dd066db30642b610c624709912d1
bp64 <scratchpeterson@gmail.com>
2022-06-05 20:11:14 -0500

    update __init__.py

--- lightweight_gan/__init__.py
+++ lightweight_gan/__init__.py
@@ -1,7 +1,3 @@
-from lightweight_gan.lightweight_gan import (
-    Discriminator,
-    Generator,
-    LightweightGAN,
-    NanException,
-    Trainer,
-)
+from lightweight_gan.exceptions import NanException
+from lightweight_gan.lightweight_gan import Discriminator, Generator, LightweightGAN
+from lightweight_gan.trainer import Trainer
