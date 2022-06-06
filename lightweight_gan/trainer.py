1aa1e5e3595278fcbcd1f4a7ccb9513b773ac169
bp64 <scratchpeterson@gmail.com>
2022-06-05 20:31:51 -0500

    moved dataset out of trainer file

--- lightweight_gan/trainer.py
+++ lightweight_gan/trainer.py
@@ -2,7 +2,6 @@ import json
 import math
 import multiprocessing
 import os
-from functools import partial
 from math import floor
 from pathlib import Path
 from random import random
@@ -16,10 +15,11 @@ from torch import nn
 from torch.autograd import grad as torch_grad
 from torch.cuda.amp import GradScaler, autocast
 from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
 from torchvision import transforms
 
+from lightweight_gan.dataset import ImageDataset
 from lightweight_gan.exceptions import NanException
 from lightweight_gan.lightweight_gan import LightweightGAN
 from lightweight_gan.loss_fns import dual_contrastive_loss, gen_hinge_loss, hinge_loss
@@ -41,117 +41,7 @@ from lightweight_gan.version import __version__
 
 # constants
 
-EXTS = ["jpg", "jpeg", "png"]
 NUM_CORES = multiprocessing.cpu_count()
-
-
-# dataset
-
-
-def convert_image_to(img_type, image):
-    if image.mode != img_type:
-        return image.convert(img_type)
-    return image
-
-
-class identity(object):
-    def __call__(self, tensor):
-        return tensor
-
-
-class expand_greyscale(object):
-    def __init__(self, transparent):
-        self.transparent = transparent
-
-    def __call__(self, tensor):
-        channels = tensor.shape[0]
-        num_target_channels = 4 if self.transparent else 3
-
-        if channels == num_target_channels:
-            return tensor
-
-        alpha = None
-        if channels == 1:
-            color = tensor.expand(3, -1, -1)
-        elif channels == 2:
-            color = tensor[:1].expand(3, -1, -1)
-            alpha = tensor[1:]
-        else:
-            raise Exception(f"image with invalid number of channels given {channels}")
-
-        if not exists(alpha) and self.transparent:
-            alpha = torch.ones(1, *tensor.shape[1:], device=tensor.device)
-
-        return color if not self.transparent else torch.cat((color, alpha))
-
-
-def resize_to_minimum_size(min_size, image):
-    if max(*image.size) < min_size:
-        return torchvision.transforms.functional.resize(image, min_size)
-    return image
-
-
-class RandomApply(nn.Module):
-    def __init__(self, prob, fn, fn_else=lambda x: x):
-        super().__init__()
-        self.fn = fn
-        self.fn_else = fn_else
-        self.prob = prob
-
-    def forward(self, x):
-        fn = self.fn if random() < self.prob else self.fn_else
-        return fn(x)
-
-
-class ImageDataset(Dataset):
-    def __init__(
-        self, folder, image_size, transparent=False, greyscale=False, aug_prob=0.0
-    ):
-        super().__init__()
-        self.folder = folder
-        self.image_size = image_size
-        self.paths = [p for ext in EXTS for p in Path(f"{folder}").glob(f"**/*.{ext}")]
-        assert len(self.paths) > 0, f"No images were found in {folder} for training"
-
-        if transparent:
-            num_channels = 4
-            pillow_mode = "RGBA"
-            expand_fn = expand_greyscale(transparent)
-        elif greyscale:
-            num_channels = 1
-            pillow_mode = "L"
-            expand_fn = identity()
-        else:
-            num_channels = 3
-            pillow_mode = "RGB"
-            expand_fn = expand_greyscale(transparent)
-
-        convert_image_fn = partial(convert_image_to, pillow_mode)
-
-        self.transform = transforms.Compose(
-            [
-                transforms.Lambda(convert_image_fn),
-                transforms.Lambda(partial(resize_to_minimum_size, image_size)),
-                transforms.Resize(image_size),
-                RandomApply(
-                    aug_prob,
-                    transforms.RandomResizedCrop(
-                        image_size, scale=(0.5, 1.0), ratio=(0.98, 1.02)
-                    ),
-                    transforms.CenterCrop(image_size),
-                ),
-                transforms.ToTensor(),
-                transforms.Lambda(expand_fn),
-            ]
-        )
-
-    def __len__(self):
-        return len(self.paths)
-
-    def __getitem__(self, index):
-        path = self.paths[index]
-        img = Image.open(path)
-        return self.transform(img)
 
 
 def gradient_accumulate_contexts(gradient_accumulate_every, is_ddp, ddps):
