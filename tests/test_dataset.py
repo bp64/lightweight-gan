ce20071c14019a4139563c7b9654ae2b85430dcb
bp64 <scratchpeterson@gmail.com>
2022-06-08 14:36:53 -0500

    tests init

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,5 +1,36 @@
+import pytest
 from lightweight_gan.dataset import ImageDataset
 
 
-def test_dataset():
+@pytest.fixture
+def jpeg_image():
+    jpg = BytesIO()
+    im = Image.new("RGB", (100, 200), color=200)
+    im.save(jpg, format="jpeg")
+    yield jpg
+    im.close()
+
+
+@pytest.fixture
+def png_image():
+    png = BytesIO()
+    im = Image.new("RGB", (100, 200), color=200)
+    im.save(png, format="png")
+    yield png
+    png.close()
+
+
+@pytest.fixture
+def create_png_data_dir(tmp_path_factory):
+    data_dir = tmp_path_factory.mktemp("png")
+    yield data_dir.as_posix()
+
+
+@pytest.fixture
+def png_jpg_images(png_image, jpeg_image):
+    yield png_image, jpeg_image
+
+
+def test_dataset_opens_jpeg(jpeg_image):
+    ImageDataset()
     assert True
